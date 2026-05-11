4d817ebca576b403ccfe337cd5ec507dd9731432
Simon Onos <simononos818@gmail.com>
2026-05-11 21:58:56 +0100

    Remove static export

--- next.config.js
+++ next.config.js
@@ -1,12 +1,1 @@
-const path = require('path');
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  distDir: 'out',
-  images: { unoptimized: true },
-  trailingSlash: true,
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
-  outputFileTracingRoot: path.join(__dirname),
-}
-module.exports = nextConfig
+
