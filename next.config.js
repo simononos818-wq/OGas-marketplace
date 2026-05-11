8af30f0aea002d741cee8541f5f19ba07f130855
Simon Onos <simononos818@gmail.com>
2026-05-11 22:18:48 +0100

    Fix next.config.js for production SSR

--- next.config.js
+++ next.config.js
@@ -1,1 +1,12 @@
+const path = require('path');
 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: { unoptimized: true },
+  trailingSlash: true,
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  outputFileTracingRoot: path.join(__dirname),
+};
+
+module.exports = nextConfig;
