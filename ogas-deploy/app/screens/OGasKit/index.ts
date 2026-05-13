161d17d0fce0d593b9c093e2f8a047ecdcbc83fb
Simon Onos <simononos818@gmail.com>
2026-05-13 15:02:21 +0100

    Fix: Remove duplicate AuthContext files causing build failure

--- ogas-deploy/app/screens/OGasKit/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as StarterKitHomeScreen } from './StarterKitHomeScreen';
-export { default as SmartCalculatorScreen } from './SmartCalculatorScreen';
-export { default as CheckoutScreen } from './CheckoutScreen';
-export * from './OGasKitData';
--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import OGASMarketplace from "./buyer/page";
-
-export default function HomePage() {
-  return <OGASMarketplace />;
-}
